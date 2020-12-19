2b03b9acc288cb17a82db0091ecea51e6747f668
ilpalbio <ilpalbio@gmail.com>
2020-12-19 17:30:38 +0100

    add riga 4

--- prova1.py
+++ prova1.py
@@ -1,3 +1,4 @@
 print('ciao questo è il primo commit su questo file')
 print('secondo commit su questo file nel branch master')
 print('tezo commit nel branch master')
+print('primo commit nel branch branch2')
